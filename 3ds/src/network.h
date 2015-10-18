6fdcd75c19b8d3c11fc4834f74c76323b7357b92
Eric Ahn <wchill1337@gmail.com>
2015-10-18 06:19:29 -0400

    Working except for dynamic usernames

--- 3ds/src/network.h
+++ 3ds/src/network.h
@@ -9,16 +9,21 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #include "inet_pton.h"
 
 extern int sock;
-extern struct sockaddr_in sain, saout;
+extern struct sockaddr_in server, saout;
 extern char outBuf[128], rcvBuf[128];
 
 extern socklen_t sockaddr_in_sizePtr;
 
-bool openSocket(int port);
+bool openSocket(char *addr, int port);
+void closeSocket();
+u32 sendBuf(void *buffer, size_t count);
+/*
 void sendBuf(int length);
 int receiveBuffer(int length);
 void sendKeys(unsigned int keys, circlePosition circlePad, circlePosition cStick, touchPosition touch);
+*/
