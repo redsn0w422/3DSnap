bdd4d3f86f0c518aa5ed238c56ef28d45ac28f01
Eric Ahn <wchill1337@gmail.com>
2015-10-17 20:56:09 -0400

    stereoscopic rendering

--- 3ds/src/main.c
+++ 3ds/src/main.c
@@ -20,6 +20,8 @@
 
 static jmp_buf exitJmp;
 
+static char buffer[128];
+
 void hang(char *message) {
     while (aptMainLoop()) {
         hidScanInput();
@@ -52,6 +54,120 @@ void displayMessage(int x, int y, char *msg) {
     drawString(x, y, msg);
     gfxFlushBuffers();
     gfxSwapBuffers();
+}
+
+/*
+r = (uint8_t) ((data >> 11) & 0x1F) << 3;
+r |= (r >> 5);
+g = (uint8_t) ((data >> 5) & 0x3F) << 2;
+g |= (g >> 6);
+b = (uint8_t) (data & 0x1F) << 3;
+b |= (b >> 5);
+*/
+
+void writePictureToFramebufferRGB565(void *fb, u16 *img) {
+    u8 *framebuf = (u8*) fb;
+    int i, j, x, y;
+    for(j = 0; j < 240; j++) {
+        for(i = 0; i < 400; i++) {
+            y = 240 - j;
+            x = i;
+            u32 v = (y + x * 240) * 3;
+            u16 data = img[j * 400 + i];
+            uint8_t b = ((data >> 11) & 0x1F) << 3;
+            uint8_t g = ((data >> 5) & 0x3F) << 2;
+            uint8_t r = (data & 0x1F) << 3;
+            framebuf[v] = r;
+            framebuf[v+1] = g;
+            framebuf[v+2] = b;
+        }
+    }
+}
+
+void writePictureToFramebufferRGB888(void *fb, void *img) {
+    memcpy(fb, img, 400*240*3);
+}
+
+u8* takePicture3D() {
+    u16 width = 400;
+    u16 height = 240;
+    u32 screen_size = width * height * 2;
+    u32 mem_size = screen_size * 2;
+    CAMU_Size size = SIZE_CTR_TOP_LCD;
+    CAMU_CameraSelect select = SELECT_OUT1_OUT2;
+
+    u32 bufSize;
+    printf("CAMU_GetMaxBytes: 0x%08X\n", (unsigned int) CAMU_GetMaxBytes(&bufSize, width, height));
+    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_CAM1, bufSize, width, height));
+    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_CAM2, bufSize, width, height));
+
+    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(select));
+
+    u8* buf = (u8*) malloc(mem_size);
+
+    Handle camReceiveEvent = 0;
+    Handle camReceiveEvent2 = 0;
+
+    printf("CAMU_ClearBuffer: 0x%08X\n", (unsigned int) CAMU_ClearBuffer(PORT_CAM1));
+    printf("CAMU_ClearBuffer: 0x%08X\n", (unsigned int) CAMU_ClearBuffer(PORT_CAM2));
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, width * height * 2, (s16) bufSize));
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent2, buf + width * height * 2, PORT_CAM2, width * height * 2, (s16) bufSize));
+    printf("CAMU_StartCapture: 0x%08X\n", (unsigned int) CAMU_StartCapture(PORT_CAM1));
+    printf("CAMU_StartCapture: 0x%08X\n", (unsigned int) CAMU_StartCapture(PORT_CAM2));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent2, U64_MAX));
+
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, width * height * 2, (s16) bufSize));
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent2, buf + width * height * 2, PORT_CAM2, width * height * 2, (s16) bufSize));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent2, U64_MAX));
+
+    printf("CAMU_StopCapture: 0x%08X\n", (unsigned int) CAMU_StopCapture(PORT_CAM1));
+    printf("CAMU_StopCapture: 0x%08X\n", (unsigned int) CAMU_StopCapture(PORT_CAM2));
+    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_NONE));
+
+    gfxFlushBuffers();
+    gspWaitForVBlank();
+    gfxSwapBuffers();
+
+    return buf;
+}
+
+u8* takePictureInner() {
+    u16 width = 400;
+    u16 height = 240;
+    u32 screen_size = width * height * 2;
+    u32 mem_size = screen_size;
+    CAMU_Size size = SIZE_CTR_TOP_LCD;
+    CAMU_CameraSelect select = SELECT_IN1;
+
+    u32 bufSize;
+    printf("CAMU_GetMaxBytes: 0x%08X\n", (unsigned int) CAMU_GetMaxBytes(&bufSize, width, height));
+    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_CAM1, bufSize, width, height));
+    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_CAM2, bufSize, width, height));
+
+    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(select));
+
+    u8* buf = (u8*) malloc(mem_size);
+
+    Handle camReceiveEvent = 0;
+
+    printf("CAMU_ClearBuffer: 0x%08X\n", (unsigned int) CAMU_ClearBuffer(PORT_CAM1));
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, width * height * 2, (s16) bufSize));
+    printf("CAMU_StartCapture: 0x%08X\n", (unsigned int) CAMU_StartCapture(PORT_CAM1));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
+
+    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, width * height * 2, (s16) bufSize));
+    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
+
+    printf("CAMU_StopCapture: 0x%08X\n", (unsigned int) CAMU_StopCapture(PORT_CAM1));
+    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_NONE));
+
+    gfxFlushBuffers();
+    gspWaitForVBlank();
+    gfxSwapBuffers();
+
+    return buf;
 }
 
 int main() {
@@ -92,54 +208,26 @@ int main() {
 
     clearScreen();
 
-    char buffer[128];
-
+    u16 width = 400;
+    u16 height = 240;
+    u32 screen_size = width * height * 2;
+    u32 mem_size = screen_size * 2;
+    CAMU_Size size = SIZE_CTR_TOP_LCD;
+    CAMU_CameraSelect select = SELECT_OUT1_OUT2;
     printf("camInit: 0x%08X\n", (unsigned int) camInit());
 
-    printf("CAMU_SetSize: 0x%08X\n", (unsigned int) CAMU_SetSize(SELECT_OUT1, SIZE_DS_LCDx4, CONTEXT_A));
-    printf("CAMU_SetOutputFormat: 0x%08X\n", (unsigned int) CAMU_SetOutputFormat(SELECT_OUT1, OUTPUT_RGB_565, CONTEXT_A));
+    printf("CAMU_SetSize: 0x%08X\n", (unsigned int) CAMU_SetSize(select, size, CONTEXT_A));
+    printf("CAMU_SetOutputFormat: 0x%08X\n", (unsigned int) CAMU_SetOutputFormat(select, OUTPUT_RGB_565, CONTEXT_A));
 
-    printf("CAMU_SetNoiseFilter: 0x%08X\n", (unsigned int) CAMU_SetNoiseFilter(SELECT_OUT1, true));
-    printf("CAMU_SetAutoExposure: 0x%08X\n", (unsigned int) CAMU_SetAutoExposure(SELECT_OUT1, true));
-    printf("CAMU_SetAutoWhiteBalance: 0x%08X\n", (unsigned int) CAMU_SetAutoWhiteBalance(SELECT_OUT1, true));
+    printf("CAMU_SetNoiseFilter: 0x%08X\n", (unsigned int) CAMU_SetNoiseFilter(select, true));
+    printf("CAMU_SetAutoExposure: 0x%08X\n", (unsigned int) CAMU_SetAutoExposure(select, true));
+    printf("CAMU_SetAutoWhiteBalance: 0x%08X\n", (unsigned int) CAMU_SetAutoWhiteBalance(select, true));
 
-    printf("CAMU_SetTrimming: 0x%08X\n", (unsigned int) CAMU_SetTrimming(PORT_CAM1, true));
-    printf("CAMU_SetTrimmingParamsCenter: 0x%08X\n", (unsigned int) CAMU_SetTrimmingParamsCenter(PORT_CAM1, 512, 240, 512, 384));
+    printf("CAMU_SetTrimming: 0x%08X\n", (unsigned int) CAMU_SetTrimming(PORT_CAM1, false));
+    printf("CAMU_SetTrimming: 0x%08X\n", (unsigned int) CAMU_SetTrimming(PORT_CAM2, false));
+    //printf("CAMU_SetTrimmingParamsCenter: 0x%08X\n", (unsigned int) CAMU_SetTrimmingParamsCenter(PORT_CAM1, 512, 240, 512, 384));
 
-    u32 bufSize;
-    printf("CAMU_GetMaxBytes: 0x%08X\n", (unsigned int) CAMU_GetMaxBytes(&bufSize, 512, 240));
-    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_CAM1, bufSize, 512, 240));
-
-    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_OUT1));
-
-    u8* buf = (u8*) malloc(bufSize * 48);
-
-    Handle camReceiveEvent = 0;
-
-    printf("CAMU_ClearBuffer: 0x%08X\n", (unsigned int) CAMU_ClearBuffer(PORT_CAM1));
-    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, bufSize * 48, (s16) bufSize));
-    printf("CAMU_StartCapture: 0x%08X\n", (unsigned int) CAMU_StartCapture(PORT_CAM1));
-    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
-
-    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, bufSize * 48, (s16) bufSize));
-    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, U64_MAX));
-
-    printf("CAMU_StopCapture: 0x%08X\n", (unsigned int) CAMU_StopCapture(PORT_CAM1));
-    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_NONE));
-
-    FILE* fd = fopen("/out.bin", "wb");
-    if(fd) {
-        printf("Writing file.\n");
-        fwrite(buf, 1, bufSize * 48, fd);
-        fclose(fd);
-        printf("Wrote file.\n");
-    } else {
-        printf("Failed to open file.\n");
-    }
-
-    printf("Freeing buf.\n");
-    free(buf);
-    printf("Freed buf.\n");
+    u8 *buf = takePicture();
 
     gfxFlushBuffers();
     gspWaitForVBlank();
@@ -204,11 +292,22 @@ int main() {
             drawString(135, 30, "RIGHT");
         }
 
+        if(CONFIG_3D_SLIDERSTATE > 0.0f) {
+            gfxSet3D(true);
+            writePictureToFramebufferRGB565Left(gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL), buf);
+            writePictureToFramebufferRGB565Right(gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL), buf + screen_size);
+            //writePictureToFramebufferRGB565Left(gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL), buf, (u16) offset);
+            //writePictureToFramebufferRGB565Right(gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL), buf + screen_size, (u16) offset);
+        } else {
+            gfxSet3D(false);
+            writePictureToFramebufferRGB565Left(gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL), buf);
+        }
+
         drawString(20, 50, itoa(circlePad.dx, buffer, 10));
         drawString(55, 50, itoa(circlePad.dy, buffer, 10));
         drawString(20, 60, itoa(cStick.dx, buffer, 10));
         drawString(55, 60, itoa(cStick.dy, buffer, 10));
-        sprintf(buffer, "%f", slider3D);
+        sprintf(buffer, "%f %f", slider3D, offset);
         drawString(20, 70, buffer);
 
         if(touch.px && touch.py) {
@@ -230,6 +329,7 @@ int main() {
     }
 
     // Exit
+    free(buf);
     cleanup();
 
     // Return to hbmenu
