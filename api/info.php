cc1f027862e4ab52b88a360e60e719df683a3aa1
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-10-17 20:25:12 -0400

    actual picture api

--- api/info.php
+++ api/info.php
@@ -1,3 +1,19 @@
 <?php
-echo phpinfo();
+$url1 = $_GET['url1'];
+$url2 = $_GET['url2'];
+
+$im = new Imagick();
+
+$im1 = new Imagick($url1);
+$im2 = new Imagick($url2);
+
+$im->addImage($im1);
+$im->addImage($im2);
+
+$im->resetIterator();
+$combined = $im->appendImages(false);
+
+$combined->setImageFormat('jpg');
+header('Content-Type: image/jpg');
+echo $combined;
 ?>
